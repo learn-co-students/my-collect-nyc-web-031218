eb681c46ee336c60d71b3bba2bc82da1a9b33116
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-27 19:07:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+    i=0
+    arr = []
+    while i < array.length
+      yield(array[i])
+      arr << yield(array[i])
+      i+=1
+    end
+    arr
+  else
+    arrays
+  end
+end
