3cd41745c2839299e2e424440282142dd0732d1e
Arthur Lurye <arthurlurye@gmail.com>
2018-03-01 09:03:54 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,29 @@
+# def my_collect(array)
+# i = 0
+# newArray = []
+#
+# while i < array.length
+#     yield array[i]
+#      newArray.push(array[i])
+#   i = i + 1
+# end
+# newArray
+# end
+
+def my_collect(array)
+i = 0
+
+newArray = []
+while i < array.length
+
+      newArray.push(yield array[i])
+  i = i + 1
+end
+newArray
+end
 
 
+# collection = ['ruby', 'javascript', 'python', 'objective-c']
+# my_collect(collection) do |lang|
+#   lang.upcase
+# end
